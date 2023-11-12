7868c42e6ac162dc612c8c4d1c9ba9185a6223d4
elusivesmw <78516603+elusivesmw@users.noreply.github.com>
2023-11-11 20:11:04 -0800

    refactor: pull multichat out of page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,9 @@
-"use client";
 import Following from "@/components/following";
 import Player from "@/components/player";
-import Chat from "@/components/chat";
-import { LeftArrow, RightArrow } from "@/components/icons";
-import { useState } from "react";
+import MultiChat from "@/components/chat";
 
 export default function Home() {
-  const channels = ["louisdoucet", "firstnamebutt"];
-
-  const [visibleIndex, setVisibleIndex] = useState(0);
-
-  const next = () => {
-    console.log("next");
-    var i = visibleIndex + 1;
-    if (i > channels.length - 1) i = 0;
-    setVisibleIndex(i);
-  };
-
-  const prev = () => {
-    console.log("prev");
-    var i = visibleIndex - 1;
-    if (i < 0) i = channels.length - 1;
-    setVisibleIndex(i);
-  };
+  const channels = ["iamtheratio", "firstnamebutt", "yourboyrudy"];
 
   return (
     <main className="flex max-h-screen justify-between">
@@ -32,25 +13,7 @@ export default function Home() {
           <Player channel={e} key={i} />
         ))}
       </div>
-
-      <div className="flex flex-col h-screen w-[350px]">
-        <div className="flex h-[50px] flex-wrap bg-twitchbg border-b border-twitchborder text-twitchtext items-center justify-between">
-          <div onClick={prev} className="h-[20px] px-1 cursor-pointer">
-            <LeftArrow />
-          </div>
-          <span className="uppercase font-bold text-xs text-center">
-            {channels[visibleIndex]}
-          </span>
-          <div onClick={next} className="h-[20px] px-1 cursor-pointer">
-            <RightArrow />
-          </div>
-        </div>
-        <div className="flex grow h-auto">
-          {channels.map((e, i) => (
-            <Chat channel={e} visible={i == visibleIndex} key={i} />
-          ))}
-        </div>
-      </div>
+      <MultiChat channels={channels} />
     </main>
   );
 }
--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,3 +1,53 @@
+"use client";
+
+import { useState } from "react";
+import { LeftArrow, RightArrow } from "@/components/icons";
+
+const MultiChat = ({ channels }: { channels: string[] }) => {
+  const [visibleIndex, setVisibleIndex] = useState(0);
+
+  const next = () => {
+    console.log("next");
+    var i = visibleIndex + 1;
+    if (i > channels.length - 1) i = 0;
+    setVisibleIndex(i);
+  };
+
+  const prev = () => {
+    console.log("prev");
+    var i = visibleIndex - 1;
+    if (i < 0) i = channels.length - 1;
+    setVisibleIndex(i);
+  };
+
+  return (
+    <div className="flex flex-col h-screen w-[350px]">
+      <div className="flex h-[50px] flex-wrap bg-twitchbg border-b border-twitchborder text-twitchtext items-center justify-between">
+        <div
+          onClick={prev}
+          className="h-[20px] px-1 cursor-pointer select-none"
+        >
+          <LeftArrow />
+        </div>
+        <span className="uppercase font-bold text-xs text-center">
+          {channels[visibleIndex]}
+        </span>
+        <div
+          onClick={next}
+          className="h-[20px] px-1 cursor-pointer select-none"
+        >
+          <RightArrow />
+        </div>
+      </div>
+      <div className="flex grow h-auto">
+        {channels.map((e, i) => (
+          <Chat channel={e} visible={i == visibleIndex} key={i} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
 const Chat = ({ channel, visible }: { channel: string; visible: boolean }) => {
   return (
     <div className={`${visible ? "block" : "hidden"}`}>
@@ -12,4 +62,4 @@ const Chat = ({ channel, visible }: { channel: string; visible: boolean }) => {
   );
 };
 
-export default Chat;
+export default MultiChat;
