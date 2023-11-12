39bb1ede091ec2a4488e2661bcc6aa57ae4a3ec5
elusivesmw <78516603+elusivesmw@users.noreply.github.com>
2023-11-12 09:27:35 -0800

    click channel to add video/chat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,26 @@
+"use client";
+
 import Following from "@/components/following";
 import Player from "@/components/player";
 import MultiChat from "@/components/chat";
+import { useState } from "react";
 
 export default function Home() {
-  const channels = ["iamtheratio", "firstnamebutt", "yourboyrudy"];
+  const [watching, setWatching] = useState(["katun24"]);
+  const addWatching = (channel: string) => {
+    if (watching.includes(channel)) return;
+    setWatching([...watching, channel]);
+  };
 
   return (
     <main className="flex max-h-screen justify-between">
-      <Following />
+      <Following setWatching={addWatching} />
       <div className="flex flex-col h-screen basis-auto grow shrink">
-        {channels.map((e, i) => (
+        {watching.map((e, i) => (
           <Player channel={e} key={i} />
         ))}
       </div>
-      <MultiChat channels={channels} />
+      <MultiChat channels={watching} />
     </main>
   );
 }
--- src/components/following.tsx
+++ src/components/following.tsx
@@ -1,9 +1,32 @@
-const Following = () => {
+"use client";
+
+import React from "react";
+
+const channels = [
+  "katun24",
+  "barbarousking",
+  "dawildgrim",
+  "dunkorslam",
+  "yourboyrudy",
+  "caioskii",
+];
+const Following = ({
+  setWatching,
+}: {
+  setWatching: (channel: string) => void;
+}) => {
   return (
-    <div className="self-stretch bg-white/100 w-12">
-      <span className="rotate-180" style={{ writingMode: "vertical-rl" }}>
-        following...
-      </span>
+    <div className="flex flex-col flex-wrap bg-twitchbg w-[240px]">
+      <div className="flex h-[50px] text-center items-center justify-center">
+        <span className="uppercase font-bold text-xs">Followed Channels</span>
+      </div>
+      {channels.map((e, i) => (
+        <div className="px-[10px] py-[5px]" onClick={() => setWatching(e)}>
+          <span className="h-[42px]" key={i}>
+            {e}
+          </span>
+        </div>
+      ))}
     </div>
   );
 };
