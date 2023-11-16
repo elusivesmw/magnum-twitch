7e2ece07df4fd7f4e8414cc3d157e17938248afa
elusivesmw <78516603+elusivesmw@users.noreply.github.com>
2023-11-16 03:24:48 -0800

    profile images

--- src/components/following.tsx
+++ src/components/following.tsx
@@ -23,6 +23,19 @@ interface Stream {
   viewer_count: number;
 }
 
+interface User {
+  id: string;
+  login: string;
+  display_name: string;
+  type: string;
+  broadcaster_type: string;
+  description: string;
+  profile_image_url: string;
+  offline_image_url: string;
+  view_count: number;
+  created_at: string;
+}
+
 const Following = ({
   addWatching,
 }: {
@@ -44,6 +57,14 @@ const Following = ({
     });
   }, [access_token]);
 
+  let [users, setUsers] = useState<User[]>([]);
+  useEffect(() => {
+    if (!access_token) return;
+    GetUsers(access_token, channels).then((res) => {
+      setUsers(res);
+    });
+  }, [channels]);
+
   return (
     <div className="flex flex-col flex-wrap bg-twitchbg w-[240px]">
       <div className="flex h-[50px] text-center items-center justify-center">
@@ -58,29 +79,40 @@ const Following = ({
           </a>
         </span>
       )}
-      {channels.map((e, i) => {
-        console.log(e);
+      {channels.map((channel, i) => {
+        let user = users.find((u) => u.id == channel.user_id);
+
         return (
           <div
-            className="px-[10px] py-[5px] cursor-pointer hover:bg-twitchbghover"
-            onClick={() => addWatching(e.user_login)}
+            className="flex px-[10px] py-[5px] cursor-pointer hover:bg-twitchbghover"
+            onClick={() => addWatching(channel.user_login)}
+            key={i}
           >
-            <div className="flex justify-between content-center">
+            <div className="h-[30px] max-w-[30px]">
+              {user && (
+                <img
+                  src={user.profile_image_url}
+                  alt={`${channel.user_name} profile`}
+                  className="w-full max-w-full rounded-2xl object-cover"
+                />
+              )}
+            </div>
+            <div className="flex justify-between content-center ml-[10px] grow">
               <div className="">
                 <span
-                  className="line-block text-[#dedee3] text-sm font-semibold h-[42px]"
+                  className="block text-[#dedee3] text-sm font-semibold"
                   key={i}
                 >
-                  {e.user_name}
+                  {channel.user_name}
                 </span>
-                <span className="block text-xs text-twitchfadedtext">
-                  {e.game_name}
+                <span className="inline-block text-xs text-twitchfadedtext">
+                  {channel.game_name}
                 </span>
               </div>
-              <div className="inline-block">
+              <div className="inline-block ml-[5px]">
                 <div className="flex items-center">
                   <div className="inline-block bg-[#eb0400] h-[8px] w-[8px] rounded rl-[5px]"></div>
-                  <div className="text-xs ml-[5px]">{e.viewer_count}</div>
+                  <div className="text-xs ml-[5px]">{channel.viewer_count}</div>
                 </div>
               </div>
             </div>
@@ -116,6 +148,30 @@ const GetFollowedChannels = (access_token: string): Promise<Stream[]> => {
     .then((res) => res.json())
     .then((json) => {
       return json.data as Stream[];
+    });
+  return data;
+};
+
+const GetUsers = (
+  access_token: string,
+  channels: Stream[]
+): Promise<User[]> => {
+  let ids = channels.map((c) => c.user_id);
+  let ids_param = 'id=' + ids.join('&id=');
+
+  const httpOptions: Object = {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+      'Client-Id': TWITCH_CLIENT_ID,
+    },
+  };
+  let data = fetch(
+    `https://api.twitch.tv/helix/users?${ids_param}`,
+    httpOptions
+  )
+    .then((res) => res.json())
+    .then((json) => {
+      return json.data as User[];
     });
   return data;
 };
