ef5d1a347002fcd2d944bbf53389784bafcef5c4
elusivesmw <78516603+elusivesmw@users.noreply.github.com>
2023-11-16 15:09:51 -0800

    refactor: following component

--- src/components/following.tsx
+++ src/components/following.tsx
@@ -40,49 +40,77 @@ interface User {
 const Following = ({
   addWatching,
 }: {
-  addWatching: (channel: string) => void;
+  addWatching: (stream: string) => void;
 }) => {
-  let [access_token, setAccessToken] = useState(null);
+  let [accessToken, setAccessToken] = useState(null);
   useEffect(() => {
-    let hash = HashValues();
+    let hash = getHashValues();
     let token = hash.access_token;
     if (!token) return;
     setAccessToken(token);
   }, []);
 
-  let [channels, setChannels] = useState<Stream[]>([]);
+  let [streams, setStreams] = useState<Stream[]>([]);
   useEffect(() => {
-    // refactor this out
-    if (!access_token) return;
-    GetFollowedChannels(access_token).then((res) => {
-      setChannels(res);
-    });
+    updateStreams();
 
     // probably should use event sub eventually
     const intervalId = setInterval(() => {
-      if (!access_token) return;
-      GetFollowedChannels(access_token).then((res) => {
-        setChannels(res);
-      });
+      updateStreams();
     }, POLL_INTERVAL);
 
     return () => clearInterval(intervalId);
-  }, [access_token]);
+  }, [accessToken]);
 
   let [users, setUsers] = useState<User[]>([]);
   useEffect(() => {
-    if (!access_token) return;
-    GetUsers(access_token, channels).then((res) => {
-      setUsers(res);
-    });
-  }, [channels]);
+    updateUsers();
+  }, [streams]);
+
+  const updateStreams = () => {
+    if (!accessToken) return;
+    const httpOptions: Object = {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'Client-Id': TWITCH_CLIENT_ID,
+      },
+    };
+    fetch(
+      `https://api.twitch.tv/helix/streams/followed?user_id=${USER_ID}&first=100`,
+      httpOptions
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        let streams = json.data as Stream[];
+        setStreams(streams);
+      });
+  };
+
+  const updateUsers = () => {
+    if (!accessToken) return;
+    let ids = streams.map((s) => s.user_id);
+    let ids_param = 'id=' + ids.join('&id=');
+
+    const httpOptions: Object = {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'Client-Id': TWITCH_CLIENT_ID,
+      },
+    };
+    fetch(`https://api.twitch.tv/helix/users?${ids_param}`, httpOptions)
+      .then((res) => res.json())
+      .then((json) => {
+        let users = json.data as User[];
+        setUsers(users);
+      });
+  };
 
   return (
     <div className="flex flex-col flex-wrap bg-sidepanel w-[240px]">
       <div className="flex h-[50px] max-w-full text-center items-center justify-center">
         <span className="uppercase font-bold text-sm">Followed Channels</span>
       </div>
-      {!access_token && (
+      {!accessToken && (
         <span className="px-[10px] py-[5px]">
           <a
             href={`https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=${TWITCH_CLIENT_ID}&redirect_uri=http://localhost:3000&scope=user:read:follows`}
@@ -91,20 +119,20 @@ const Following = ({
           </a>
         </span>
       )}
-      {channels.map((channel, i) => {
-        let user = users.find((u) => u.id == channel.user_id);
+      {streams.map((stream, i) => {
+        let user = users.find((u) => u.id == stream.user_id);
 
         return (
           <div
             className="flex max-w-full px-4 py-2 cursor-pointer hover:bg-sidepanelhover"
-            onClick={() => addWatching(channel.user_login)}
+            onClick={() => addWatching(stream.user_login)}
             key={i}
           >
             <div className="basis-[30px] grow-0 shrink-0 self-center">
               {user && (
                 <img
                   src={user.profile_image_url}
-                  alt={`${channel.user_name} profile`}
+                  alt={`${stream.user_name} profile`}
                   className="w-full max-w-full rounded-full object-cover"
                 />
               )}
@@ -112,17 +140,17 @@ const Following = ({
             <div className="flex justify-between content-center ml-2 grow min-w-0">
               <div className="flex flex-col shrink overflow-hidden">
                 <p className="text-[#dedee3] text-base font-semibold truncate">
-                  {channel.user_name}
+                  {stream.user_name}
                 </p>
                 <p className="text-sm text-twfadedtext truncate">
-                  {channel.game_name}
+                  {stream.game_name}
                 </p>
               </div>
               <div className="inline-block min-w-[40px] ml-2">
                 <div className="flex items-center">
                   <div className="inline-block bg-[#eb0400] h-[8px] w-[8px] rounded-full rl-2"></div>
                   <div className="text-sm ml-2">
-                    {ViewerCountDisplay(channel.viewer_count)}
+                    {displayViewerCount(stream.viewer_count)}
                   </div>
                 </div>
               </div>
@@ -134,7 +162,7 @@ const Following = ({
   );
 };
 
-const HashValues = () => {
+const getHashValues = () => {
   let hash = document.location.hash.substr(1);
   var params: any = {};
   hash.split('&').map((hashkey) => {
@@ -145,49 +173,7 @@ const HashValues = () => {
   return params;
 };
 
-const GetFollowedChannels = (access_token: string): Promise<Stream[]> => {
-  const httpOptions: Object = {
-    headers: {
-      Authorization: `Bearer ${access_token}`,
-      'Client-Id': TWITCH_CLIENT_ID,
-    },
-  };
-  let data = fetch(
-    `https://api.twitch.tv/helix/streams/followed?user_id=${USER_ID}&first=100`,
-    httpOptions
-  )
-    .then((res) => res.json())
-    .then((json) => {
-      return json.data as Stream[];
-    });
-  return data;
-};
-
-const GetUsers = (
-  access_token: string,
-  channels: Stream[]
-): Promise<User[]> => {
-  let ids = channels.map((c) => c.user_id);
-  let ids_param = 'id=' + ids.join('&id=');
-
-  const httpOptions: Object = {
-    headers: {
-      Authorization: `Bearer ${access_token}`,
-      'Client-Id': TWITCH_CLIENT_ID,
-    },
-  };
-  let data = fetch(
-    `https://api.twitch.tv/helix/users?${ids_param}`,
-    httpOptions
-  )
-    .then((res) => res.json())
-    .then((json) => {
-      return json.data as User[];
-    });
-  return data;
-};
-
-const ViewerCountDisplay = (viewerCount: number) => {
+const displayViewerCount = (viewerCount: number) => {
   if (viewerCount > 1000) {
     let rounded = Number.parseFloat((viewerCount / 1000).toFixed(1)).toString();
     return `${rounded}K`;
