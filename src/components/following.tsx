aee24d567fce471aef51df05928e8e5836f97629
elusivesmw <78516603+elusivesmw@users.noreply.github.com>
2023-11-23 09:55:17 -0800

    collapsible follow sidebar

--- src/components/following.tsx
+++ src/components/following.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { Stream, User } from '@/types/twitch';
+import { CollapseLeft, CollapseRight, Heart } from './icons';
 
 const TWITCH_CLIENT_ID = process.env.NEXT_PUBLIC_TWITCH_CLIENT_ID;
 
@@ -79,50 +80,89 @@ const Following = ({
       });
   };
 
+
+  let [open, setOpen] = useState<boolean>(true);
+  const toggleOpen = () => {
+    setOpen(!open);
+  }
+
   return (
-    <div className="flex flex-col bg-sidepanel basis-[240px] shrink-0 grow-0 overflow-y-scroll scrollbar">
-      <div className="flex basis-20 shrink-0 grow-0  max-w-full text-center items-center justify-center">
-        <span className="uppercase font-bold text-sm">Followed Channels</span>
+    <div className={`flex flex-col bg-sidepanel ${open ? "basis-[240px]" : "basis-[50px]"} shrink-0 grow-0 overflow-y-scroll scrollbar`}>
+      <div className="flex shrink-0 grow-0 max-w-full text-center items-center justify-between mt-4 pl-4 pr-2">
+        <span className={`${open ? "" : "hidden"} font-semibold text-xl`}>For You</span>
+        <button
+          onClick={toggleOpen}
+          className="inline-flex h-[30px] p-2 bg-twbuttonbg hover:bg-opacity-[0.48] rounded-[4px]">
+          {open ?
+            <CollapseLeft />
+            :
+            <CollapseRight />
+          }
+        </button>
+      </div>
+      <div className={`flex basis-20 shrink-0 grow-0 max-w-full text-center items-center ${open ? "justify-start" : "justify-center"} px-4`}>
+        <span className={`${!open ? "hidden": ""} uppercase font-bold text-sm`}>Followed Channels</span>
+        <div className={`${open ? "hidden" : ""} h-8`}><Heart /></div>
       </div>
       {streams.map((stream, i) => {
         let user = users.find((u) => u.id == stream.user_id);
-
         return (
-          <div
-            className="flex max-w-full px-4 py-2 cursor-pointer hover:bg-sidepanelhover"
-            onClick={() => addWatching(stream.user_login)}
+          <StreamRow
+            stream={stream}
+            user={user}
+            open={open}
+            addWatching={addWatching}
             key={i}
-          >
-            <div className="basis-[30px] grow-0 shrink-0 self-center">
-              {user && (
-                <img
-                  src={user.profile_image_url}
-                  alt={`${stream.user_name} profile`}
-                  className="w-full max-w-full rounded-full object-cover"
-                />
-              )}
-            </div>
-            <div className="flex justify-between content-center ml-2 grow min-w-0">
-              <div className="flex flex-col shrink overflow-hidden">
-                <p className="text-[#dedee3] text-base font-semibold truncate">
-                  {stream.user_name}
-                </p>
-                <p className="text-sm text-twfadedtext truncate">
-                  {stream.game_name}
-                </p>
-              </div>
-              <div className="inline-block min-w-[40px] ml-2">
-                <div className="flex items-center">
-                  <div className="inline-block bg-[#eb0400] h-[8px] w-[8px] rounded-full rl-2"></div>
-                  <div className="text-sm ml-2">
-                    {displayViewerCount(stream.viewer_count)}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          />
         );
       })}
+    </div>
+  );
+};
+
+const StreamRow = ({
+  stream,
+  user,
+  open,
+  addWatching,
+}: {
+  stream: Stream;
+  user: User | undefined;
+  open: boolean;
+  addWatching: (stream: string) => void;
+}) => {
+  return (
+    <div
+      className="flex max-w-full px-4 py-2 cursor-pointer hover:bg-sidepanelhover"
+      onClick={() => addWatching(stream.user_login)}
+    >
+      <div className="basis-[30px] grow-0 shrink-0 self-center">
+        {user && (
+          <img
+            src={user.profile_image_url}
+            alt={`${stream.user_name} profile`}
+            className="w-full max-w-full rounded-full object-cover"
+          />
+        )}
+      </div>
+      <div className={`${!open ? "hidden" : ""} flex justify-between content-center ml-2 grow min-w-0`}>
+        <div className="flex flex-col shrink overflow-hidden">
+          <p className="text-[#dedee3] text-base font-semibold truncate">
+            {stream.user_name}
+          </p>
+          <p className="text-sm text-twfadedtext truncate">
+            {stream.game_name}
+          </p>
+        </div>
+        <div className="inline-block min-w-[40px] ml-2">
+          <div className="flex items-center">
+            <div className="inline-block bg-[#eb0400] h-[8px] w-[8px] rounded-full rl-2"></div>
+            <div className="text-sm ml-2">
+              {displayViewerCount(stream.viewer_count)}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -110,6 +110,24 @@ export const CollapseRight = ({ className = '' }: { className?: string }) => {
   );
 };
 
+export const Heart = ({ className = '' }: { className?: string }) => {
+  return (
+    <svg
+      width="100%"
+      height="100%"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+      className={className}
+    >
+      <path
+        fill-rule="evenodd"
+        d="M9.171 4.171A4 4 0 0 0 6.343 3H6a4 4 0 0 0-4 4v.343a4 4 0 0 0 1.172 2.829L10 17l6.828-6.828A4 4 0 0 0 18 7.343V7a4 4 0 0 0-4-4h-.343a4 4 0 0 0-2.829 1.172L10 5l-.829-.829zm.829 10 5.414-5.414A2 2 0 0 0 16 7.343V7a2 2 0 0 0-2-2h-.343a2 2 0 0 0-1.414.586L10 7.828 7.757 5.586A2 2 0 0 0 6.343 5H6a2 2 0 0 0-2 2v.343a2 2 0 0 0 .586 1.414L10 14.172z"
+        clip-rule="evenodd"
+      ></path>
+    </svg>
+  );
+};
+
 export const Spotlight = ({ className = '' }: { className?: string }) => {
   return (
     <svg
