d1dd8cdf4a28422fbf2c5892c70d822922ec04a0
elusivesmw <78516603+elusivesmw@users.noreply.github.com>
2024-01-06 11:44:42 -0800

    style: run prettier

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -8,36 +8,36 @@ const config: Config = {
   ],
   theme: {
     fontSize: {
-      'xs': '1.2rem',
+      xs: '1.2rem',
       sm: '1.3rem',
       base: '1.4rem',
       lg: '1.6rem',
       xl: '1.8rem',
     },
     lineHeight: {
-      'tight': '1.2'
+      tight: '1.2',
     },
     extend: {
       animation: {
-        'highlight': 'highlight 3s ease-in-out'
+        highlight: 'highlight 3s ease-in-out',
       },
       keyframes: {
         highlight: {
           '0%': { borderColor: 'var(--twpurple)' },
           '100%': { borderColor: 'black' },
-        }
-      }, 
+        },
+      },
       colors: {
-        'noplayer': 'var(--background-rgb)',
-        'sidepanel': 'rgb(31,31,35)',
-        'sidepanelhover': 'rgb(38,38,44)',
-        'chatpanel': 'rgb(24,24,27)',
-        'twpurple': 'var(--twpurple)',
-        'twborder': 'rgba(83, 83, 95, 0.48)',
-        'twtext': 'var(--foreground-rgb)',
-        'twfadedtext': '#adadb8',
-        'twbuttonbg':'rgb(83,83,95)',
-        'twbuttontext': 'rgb(239,239,241)',
+        noplayer: 'var(--background-rgb)',
+        sidepanel: 'rgb(31,31,35)',
+        sidepanelhover: 'rgb(38,38,44)',
+        chatpanel: 'rgb(24,24,27)',
+        twpurple: 'var(--twpurple)',
+        twborder: 'rgba(83, 83, 95, 0.48)',
+        twtext: 'var(--foreground-rgb)',
+        twfadedtext: '#adadb8',
+        twbuttonbg: 'rgb(83,83,95)',
+        twbuttontext: 'rgb(239,239,241)',
       },
       maxHeight: {
         '1/2': '50%',
@@ -47,8 +47,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/container-queries'),
-  ],
-}
-export default config
+  plugins: [require('@tailwindcss/container-queries')],
+};
+export default config;
