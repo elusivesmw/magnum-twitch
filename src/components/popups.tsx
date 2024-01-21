e5d9893945c07e0d27337ce99431abc714568da6
elusivesmw <78516603+elusivesmw@users.noreply.github.com>
2024-01-20 19:18:28 -0800

    add stream thumbnail to popup

--- src/components/popups.tsx
+++ src/components/popups.tsx
@@ -1,4 +1,5 @@
 import { Stream } from '@/types/twitch';
+import Image from 'next/image';
 import { createPortal } from 'react-dom';
 
 export const FollowingPopup = ({ stream }: { stream: Stream | undefined }) => {
@@ -7,14 +8,22 @@ export const FollowingPopup = ({ stream }: { stream: Stream | undefined }) => {
   let rect = target?.getBoundingClientRect();
   if (!rect) return;
 
+  //TODO: give option to not display image
+  const IMAGE_WIDTH = 220;
+  const IMAGE_HEIGHT = 124;
+
   // detect when we are near the bottom
   const BOTTOM_PADDING = 10;
   let tooltipTop: number | 'auto' = rect.top;
   let tooltipBottom: number | 'auto' = 'auto';
-  if (rect.bottom > window.innerHeight - BOTTOM_PADDING) {
+  if (rect.bottom > window.innerHeight - BOTTOM_PADDING - IMAGE_HEIGHT) {
     tooltipBottom = BOTTOM_PADDING;
     tooltipTop = 'auto';
   }
+
+  let streamThumbnail = stream.thumbnail_url
+    .replace('{width}', IMAGE_WIDTH.toString())
+    .replace('{height}', IMAGE_HEIGHT.toString());
 
   return createPortal(
     <div
@@ -25,7 +34,13 @@ export const FollowingPopup = ({ stream }: { stream: Stream | undefined }) => {
         left: rect.x + rect.width + 15,
       }}
     >
-      {stream.title}
+      <span className="block mb-2">{stream.title}</span>
+      <Image
+        src={streamThumbnail}
+        width={IMAGE_WIDTH}
+        height={IMAGE_HEIGHT}
+        alt=""
+      />
     </div>,
     document.body
   );
