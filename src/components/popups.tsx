50c75cfb3fbe57fcc68bd4c7e97220c719a45b73
elusivesmw <78516603+elusivesmw@users.noreply.github.com>
2024-01-20 19:00:22 -0800

    keep tooltip onscreen

--- src/components/popups.tsx
+++ src/components/popups.tsx
@@ -6,10 +6,24 @@ export const FollowingPopup = ({ stream }: { stream: Stream | undefined }) => {
   let target = document.getElementById(`following-stream-${stream.user_login}`);
   let rect = target?.getBoundingClientRect();
   if (!rect) return;
+
+  // detect when we are near the bottom
+  const BOTTOM_PADDING = 10;
+  let tooltipTop: number | 'auto' = rect.top;
+  let tooltipBottom: number | 'auto' = 'auto';
+  if (rect.bottom > window.innerHeight - BOTTOM_PADDING) {
+    tooltipBottom = BOTTOM_PADDING;
+    tooltipTop = 'auto';
+  }
+
   return createPortal(
     <div
       className="absolute top-20 left-20 w-96 z-50 bg-sidepanel text-white px-4 py-2 rounded-xl"
-      style={{ top: rect.top, left: rect.x + rect.width + 15 }}
+      style={{
+        top: tooltipTop,
+        bottom: tooltipBottom,
+        left: rect.x + rect.width + 15,
+      }}
     >
       {stream.title}
     </div>,
