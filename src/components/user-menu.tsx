4297a7631a44dc3439ed49adfd9a7cb903c663cf
elusivesmw <78516603+elusivesmw@users.noreply.github.com>
2024-12-07 12:49:35 -0800

    switch menu to headlessui

--- src/components/user-menu.tsx
+++ src/components/user-menu.tsx
@@ -1,9 +1,14 @@
 import Image from 'next/image';
-import { usePathname } from 'next/navigation';
-import { useEffect, useState } from 'react';
 import { External, LogOut, Settings } from './icons';
 import Link from 'next/link';
 import { User } from '@/types/twitch';
+import {
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  MenuSeparator,
+} from '@headlessui/react';
 
 export default function UserMenu({
   user,
@@ -12,16 +17,6 @@ export default function UserMenu({
   user: User | undefined;
   clearAccessToken: () => void;
 }) {
-  const [userMenuOpen, setUserMenuOpen] = useState<boolean>(false);
-  const toggleUserMenu = () => {
-    setUserMenuOpen(!userMenuOpen);
-  };
-
-  const pathname = usePathname();
-  useEffect(() => {
-    setUserMenuOpen(false);
-  }, [pathname]);
-
   if (!user) return;
 
   function logout() {
@@ -29,8 +24,8 @@ export default function UserMenu({
   }
 
   return (
-    <>
-      <div className="w-[30px] cursor-pointer" onClick={toggleUserMenu}>
+    <Menu>
+      <MenuButton className="w-[30px] cursor-pointer">
         <Image
           src={user.profile_image_url}
           alt={`${user.login} profile`}
@@ -39,12 +34,10 @@ export default function UserMenu({
           height={30}
           unoptimized
         />
-      </div>
-      <div
+      </MenuButton>
+      <MenuItems
         id="user-menu"
-        className={`${
-          userMenuOpen ? 'block' : 'hidden'
-        } absolute top-[4.5rem] right-4 z-30 flex flex-col bg-sidepanel p-4 min-w-[200px] user-menu-shadow rounded-l-xl border-r border-twbuttontext text-sm`}
+        className="absolute top-[4.5rem] right-4 z-30 flex flex-col bg-sidepanel p-4 min-w-[200px] user-menu-shadow rounded-l-xl border-r border-twbuttontext text-sm"
       >
         <div className="flex items-center p-2">
           <div className="w-[40px]">
@@ -59,11 +52,8 @@ export default function UserMenu({
           </div>
           <span className="pl-4 font-semibold">{user.display_name}</span>
         </div>
-        <div
-          role="separator"
-          className="border-t border-twborder mt-4 mx-2 pb-4"
-        ></div>
-        <div>
+        <MenuSeparator className="border-t border-twborder mt-4 mx-2 pb-4"></MenuSeparator>
+        <MenuItem>
           <Link
             href="/settings"
             className="flex p-2 rounded-lg hover:bg-twbuttonbg hover:bg-opacity-[0.48]"
@@ -73,12 +63,9 @@ export default function UserMenu({
             </div>
             <span className="">Settings</span>
           </Link>
-        </div>
-        <div
-          role="separator"
-          className="border-t border-twborder mt-4 mx-2 pb-4"
-        ></div>
-        <div>
+        </MenuItem>
+        <MenuSeparator className="border-t border-twborder mt-4 mx-2 pb-4"></MenuSeparator>
+        <MenuItem>
           <a
             href="https://www.twitch.tv/settings/connections"
             target="_blank"
@@ -89,8 +76,8 @@ export default function UserMenu({
             </div>
             <span className="">Disconnect</span>
           </a>
-        </div>
-        <div>
+        </MenuItem>
+        <MenuItem>
           <button
             onClick={logout}
             className="w-full flex p-2 rounded-lg hover:bg-twbuttonbg hover:bg-opacity-[0.48]"
@@ -100,8 +87,8 @@ export default function UserMenu({
             </div>
             <span className="">Log Out</span>
           </button>
-        </div>
-      </div>
-    </>
+        </MenuItem>
+      </MenuItems>
+    </Menu>
   );
 }
